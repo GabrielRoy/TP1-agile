c0c18caa318875342416e976fd99b0941c5cdb3c
GabrielRoy <2439055@csfoy.ca>
2026-09-14 13:56:02 -0400

    m

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/TP1-agile/',
   plugins: [
     vue(),
     vueDevTools(),
